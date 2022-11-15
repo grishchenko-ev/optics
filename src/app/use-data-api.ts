c56c63a3cf92be82ca17cd03839575bef0232728
Eugene <grishchenko.ev86@gmail.com>
2022-11-15 12:10:49 +0200

    Change api data

--- src/app/use-data-api.ts
+++ src/app/use-data-api.ts
@@ -1,6 +1,6 @@
-import * as React from 'react';
-import { useHistory } from 'react-router-dom';
-import { ListObjectsCommand, S3Client } from '@aws-sdk/client-s3';
+import * as React from "react";
+import { useHistory } from "react-router-dom";
+import { ListObjectsCommand, S3Client } from "@aws-sdk/client-s3";
 
 export const useDataApi = () => {
   const currentUrl = useHistory().location.pathname;
@@ -8,7 +8,7 @@ export const useDataApi = () => {
 
   React.useEffect(() => {
     const s3Client = new S3Client({
-      region: 'ams3',
+      region: "ams3",
       endpoint: process.env.API_URL,
       credentials: {
         accessKeyId: process.env.REACT_APP_ACCESS_KEY_ID as string,
@@ -22,9 +22,9 @@ export const useDataApi = () => {
         await s3Client
           .send(
             new ListObjectsCommand({
-              Bucket: 'optics',
-              Prefix: notMain ? currentUrl.slice(1) + '/' : undefined,
-              Delimiter: '/',
+              Bucket: "optics",
+              Prefix: notMain ? currentUrl.slice(1) + "/" : undefined,
+              Delimiter: "/",
             })
           )
           .then((data) => {
@@ -37,7 +37,7 @@ export const useDataApi = () => {
             }
           });
       } catch (err) {
-        console.log('Error', err);
+        console.log("Error", err);
       }
     };
 
@@ -46,3 +46,31 @@ export const useDataApi = () => {
 
   return urlList;
 };
+
+// Dropbox
+// const dbx = new Dropbox({
+//   accessToken:
+//     "sl.BRQjQdFuduXbLRWZYIfh3HaLB9gJFfVGI_9xOXuMSpgofB_BOk0YllWZK1Ctj4Vj8phB5qNX5wMNRc9twLYmSymQXrL2dZu1wclk0jZBC-hWit6QHYLn4evWVKrKboWTOJigptk",
+// });
+
+// export const useDataApi = () => {
+//   const currentUrl = useHistory().location.pathname;
+//   const [urlList, setUrlList] = React.useState<string[]>();
+
+//   React.useEffect(() => {
+//     dbx
+//       .filesListFolder({
+//         path: currentUrl === "/" ? "" : currentUrl,
+//       })
+//       .then(({ result }) => {
+//         if (result) {
+//           const filteredData = result.entries
+//             .filter((item) => item[".tag"] === "file")
+//             .map((item) => item.name);
+//           return setUrlList(filteredData);
+//         }
+//       });
+//   }, [currentUrl, setUrlList]);
+
+//   return urlList;
+// };
